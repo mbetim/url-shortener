b5ba5812eb4fa3c0d7576fcda38c861b4260d4e2
Matheus Betim <matheus.betim@synvia.com>
2022-04-28 20:32:10 -0300

    feat: add a form to create URLs

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,25 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
+import { nanoid } from "nanoid";
+import { z } from "zod";
+import { prisma } from "../../../shared/prisma";
 
-export const appRouter = trpc.router().query("hello", {
-  resolve() {
-    return { greeting: "Hello World!" };
+export const appRouter = trpc.router().mutation("create-url", {
+  input: z.object({ slug: z.string().nullish(), fullUrl: z.string() }),
+  async resolve({ input }) {
+    let { slug } = input;
+
+    if (slug) {
+      const isSlugInUse = await prisma.url.findFirst({ where: { slug } });
+
+      if (isSlugInUse) throw new Error("Slug already in use");
+    } else {
+      slug = nanoid(5);
+    }
+
+    const url = await prisma.url.create({ data: { slug, fullUrl: input.fullUrl } });
+
+    return { url };
   },
 });
 

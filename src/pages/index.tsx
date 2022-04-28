61256d598da5047ec9f5b2a6920b14d29822ad2f
Matheus Betim <matheus.betim@synvia.com>
2022-04-28 12:02:59 -0300

    feat: install and set up tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>Hello world</h1>
+        <h1 className="text-3xl font-bold underline">Hello world</h1>
       </main>
     </div>
   );

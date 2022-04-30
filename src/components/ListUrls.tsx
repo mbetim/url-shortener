330c562031188a207dff4ff250785276ffc82219
Matheus Betim <matheus.betim@synvia.com>
2022-04-29 21:44:50 -0300

    feat: add a copy to copy the short url

--- src/components/ListUrls.tsx
+++ src/components/ListUrls.tsx
@@ -8,17 +8,31 @@ export const ListUrls: React.FC = () => {
 
   if (error) return <p className="rounded p-3 text-red-600 bg-red-200">{error.message}</p>;
 
-  return (
-    <div className="grid grid-cols-1 gap-y-5 md:grid-cols-4 md:gap-x-5 mt-10">
-      {data?.map((url) => (
-        <a key={url.slug} href={url.fullUrl} target="_blank" rel="noreferrer">
-          <div className="shadow hover:shadow-md ease-linear duration-150 rounded p-3">
-            <h1 className="text-lg font-bold">{url.slug}</h1>
+  const copyToClipboard = (slug: string) =>
+    navigator.clipboard.writeText(window.location.href + slug);
 
+  return (
+    <div className="mt-10 grid gap-y-5 grid-cols-1 sm:grid-cols-3 sm:gap-x-5 md:grid-cols-4">
+      {data?.map((url) => (
+        <div key={url.slug} className="shadow ease-linear duration-150 rounded p-3">
+          <a href={url.fullUrl} target="_blank" rel="noreferrer">
+            <h1 className="text-lg font-bold">{url.slug}</h1>
+          </a>
+
+          <div className="my-2">
             <p className="text-sm text-black/50 break-words">{url.fullUrl}</p>
             <p className="text-sm text-black/50">Created on {url.createdAt.toDateString()}</p>
           </div>
-        </a>
+
+          <div className="flex gap-2">
+            <button
+              className="flex-1 py-1 rounded bg-blue-600 hover:bg-blue-700 text-white ease-linear duration-150"
+              onClick={() => copyToClipboard(url.slug)}
+            >
+              Copy
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
